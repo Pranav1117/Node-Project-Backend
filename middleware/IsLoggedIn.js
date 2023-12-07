5c2e054e02eef721fd483e1e0ca868f8ae0b42a6
pranav <pranavbavaskar1@gmail.com>
2023-12-07 11:51:33 +0530

    s

--- middleware/IsLoggedIn.js
+++ middleware/IsLoggedIn.js
@@ -12,37 +12,37 @@ const isLoggedIn = async (req, res) => {
 
   const { authorization } = req.headers;
 
-  // const token = authorization.split(" ")[1];
+  const token = authorization.split(" ")[1];
 
-  // if (!token) {
-  //   req.isLoggedIn = false;
-  //   req.user = null;
-  //   // console.log("In null tokenn ----------------------");
-  //   return res.send({ isLoggedIn: false });
-  // }
+  if (!token) {
+    req.isLoggedIn = false;
+    req.user = null;
+    // console.log("In null tokenn ----------------------");
+    return res.send({ isLoggedIn: false });
+  }
 
-  // try {
-  //   const { exp, email } = jwt.verify(token, SECRET_KEY);
+  try {
+    const { exp, email } = jwt.verify(token, SECRET_KEY);
 
-  //   if (exp > currentTime) {
-  //     // const User = await UserModel.findOne({ email: email });
-  //     // console.log(User.name);
-  //     req.isLoggedIn = true;
-  //     req.user = User.name;
+    if (exp > currentTime) {
+      // const User = await UserModel.findOne({ email: email });
+      // console.log(User.name);
+      req.isLoggedIn = true;
+      req.user = User.name;
 
-  //     // console.log("in 'If token in present'");
-  //     return res.send({ isLoggedIn: true });
-  //   } else {
-  //     req.isLoggedIn = false;
-  //     req.user = null;
+      // console.log("in 'If token in present'");
+      return res.send({ isLoggedIn: true });
+    } else {
+      req.isLoggedIn = false;
+      req.user = null;
 
-  //     return res.send({ isLoggedIn: false });
-  //   }
-  // } catch (err) {
-  //   console.log(err);
-  //   req.isLoggedIn = false;
-  //   req.user = null;
-  //   return res.send({ isLoggedIn: false, msg: "something went wrong" });
-  // }
+      return res.send({ isLoggedIn: false });
+    }
+  } catch (err) {
+    console.log(err);
+    req.isLoggedIn = false;
+    req.user = null;
+    return res.send({ isLoggedIn: false, msg: "something went wrong" });
+  }
 };
 module.exports = { isLoggedIn };
